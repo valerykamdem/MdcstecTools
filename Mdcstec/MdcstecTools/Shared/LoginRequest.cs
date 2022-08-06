c0f4df06eb3caf9031b249a0df01c771720f7916
Valery Kamdem <valery.kamdem@celes-systems.com>
2022-08-06 22:05:15 +0200

    Non nullable  check

--- Mdcstec/MdcstecTools/Shared/LoginRequest.cs
+++ Mdcstec/MdcstecTools/Shared/LoginRequest.cs
@@ -10,8 +10,8 @@ namespace MdcstecTools.Shared
     public class LoginRequest
     {
         [Required]
-        public string Email { get; set; }
+        public string? Email { get; set; }
         [Required]
-        public string Password { get; set; }
+        public string? Password { get; set; }
     }
 }
